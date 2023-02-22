3a012091151e207f1dc6b5034a1eb5841853e019
Vinayak Pandey <67498083+geekyvinayak@users.noreply.github.com>
2023-02-22 17:53:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function event1(){
+    document.getElementById("images").src = "item-1l.jpg";
+}
+function event2(){
+    document.getElementById("images").src = "item-2l.jpg";
+}
+function event3(){
+    document.getElementById("images").src = "item-3l.jpg";
+}
+function event4(){
+    document.getElementById("images").src = "item-4l.jpg";
+}
+function event5(){
+    document.getElementById("images").src = "item-5l.jpg";
+}
